df84746d4e964b58738ddbf45d02c99adf74e93c
Kannan Ravindran <kannan.cyberpunk@gmail.com>
2019-12-14 17:09:09 +0530

    updated homepage link

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ DB_USERNAME = "enigma"
 DB_PASSWORD = "Terminator123"
 SG_API = "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"
 ROLES = {"super": 0, "admin": 1, "feedback" :3 }
-REACT_HOST_ADDR="http://127.0.0.1:3000/"
+REACT_HOST_ADDR="http://ec2-54-218-121-175.us-west-2.compute.amazonaws.com/"
